3f8856c02b0c4b47c1d64a23de91287d4034357e
Erik Behrends <behrends@users.noreply.github.com>
2023-07-12 10:41:41 +0200

    Tableiste mit Icons gestalten

--- Navigation.jsx
+++ Navigation.jsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import * as Icon from '@expo/vector-icons';
 import HomeScreen from './screens/HomeScreen';
 import SettingsScreen from './screens/SettingsScreen';
 
@@ -12,12 +13,26 @@ export default function Navigation() {
         <Tab.Screen
           name="Home"
           component={HomeScreen}
-          options={{ title: 'Freunde' }}
+          options={{
+            title: 'Freunde',
+            tabBarIcon: ({ size, color }) => (
+              <Icon.Ionicons name="home" size={size} color={color} />
+            ),
+          }}
         />
         <Tab.Screen
           name="Settings"
           component={SettingsScreen}
-          options={{ title: 'Einstellungen' }}
+          options={{
+            title: 'Einstellungen',
+            tabBarIcon: ({ size, color }) => (
+              <Icon.Ionicons
+                name="settings"
+                size={size}
+                color={color}
+              />
+            ),
+          }}
         />
       </Tab.Navigator>
     </NavigationContainer>
