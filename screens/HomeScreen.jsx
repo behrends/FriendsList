4a972a0f307d12ba5e433987db1f9a9306f688f6
Erik Behrends <behrends@users.noreply.github.com>
2023-07-25 11:07:35 +0200

    Pull-To-Refresh in der Liste implementieren

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -12,20 +12,21 @@ export default function HomeScreen({ navigation }) {
   const [data, setData] = useState([]);
   const [isLoading, setLoading] = useState(true);
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await fetch(
-          'https://rand123omuser.me/api?results=20'
-        );
-        const json = await response.json();
-        setData(json.results);
-        setLoading(false);
-      } catch (error) {
-        alert('Fehler beim Laden');
-        setLoading(false);
-      }
+  async function fetchData() {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        'https://randomuser.me/api?results=20'
+      );
+      const json = await response.json();
+      setData(json.results);
+    } catch (error) {
+      alert('Fehler beim Laden');
     }
+    setLoading(false);
+  }
+
+  useEffect(() => {
     fetchData();
   }, []);
 
@@ -50,6 +51,8 @@ export default function HomeScreen({ navigation }) {
           />
         )}
         keyExtractor={(item) => item.email}
+        refreshing={isLoading}
+        onRefresh={fetchData}
         ItemSeparatorComponent={<View style={styles.listSeparator} />}
         ListEmptyComponent={
           <Text style={styles.listEmpty}>Keine Daten geladen</Text>
